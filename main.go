3b881d40c9c31917897b1561359c65ee1ba5d55a
abdad mubaroq <abdadmubaroq@MacBook-Air-abdad.local>
2026-08-24 15:49:13 +0700

    v1.0.0.12

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"https://dasboard-nine-gamma.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
